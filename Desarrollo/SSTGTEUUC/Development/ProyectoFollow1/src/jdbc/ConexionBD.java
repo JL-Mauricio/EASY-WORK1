bb2d3e2cd9a00c6ee46ed2be4c83e2fb93e72ff0
Alejandro Nicolás Pérez Sotomayor <114117807+alejonico@users.noreply.github.com>
2022-12-21 14:53:11 -0500

    Add files via upload

---
+++ Desarrollo/SSTGTEUUC/Development/ProyectoFollow1/src/jdbc/ConexionBD.java
@@ -0,0 +1,39 @@
+package jdbc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    public static Connection conectar() throws IOException{
+        
+        //BD REMOTA
+        /*String urlConnection = "jdbc:postgresql://ec2-3-219-52-220.compute-1.amazonaws.com:5432/d1bqrln047024d";
+        String user = "ublnbtjltkosrh";
+        Path path = Paths.get("dbremota.txt");*/
+        
+        //BD LOCAL
+        String urlConnection = "jdbc:mysql://localhost:3306/dbfollowclass";
+        String user = "root";
+
+        Path path = Paths.get("db.txt");
+
+        String password;
+        try(BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            password = br.readLine();
+            return DriverManager.getConnection(urlConnection, user, password);
+
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("VendorError: " + e.getErrorCode());
+            return null;
+        }
+    }
+}
